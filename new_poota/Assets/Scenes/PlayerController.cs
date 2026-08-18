74c434fefd933c56cfa9d3cc6df25875a4798914
Keiryu14 <ichitariyk1499@gmail.com>
2026-08-18 11:58:27 +0900

    pootaを矢印キーで横移動できるようにした

---
+++ new_poota/Assets/Scenes/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ¶–îˆó‚ª‰Ÿ‚³‚ê‚½‚Æ‚«
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+
+        // ‰E–îˆó‚ª‰Ÿ‚³‚ê‚½‚Æ‚«
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+    }
+}
