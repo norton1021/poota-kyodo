091af4e299727558abe9457bf54e438f9c42ac0f
norton1021 <renhiso1209@icloud.com>
2026-08-18 16:21:15 +0900

    HPとAttackの概念を作成した

--- new_poota/Assets/Scenes/PlayerController.cs
+++ new_poota/Assets/Scenes/PlayerController.cs
@@ -12,28 +12,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 食べ物を拾うことで減速
+        float speedy;
+        if (GetComponent<playerfood>().hasfood)
+            speedy = 0.7f;
+        else
+            speedy = 1.0f;
+
         // Aキーで左に移動
         if (Keyboard.current.aKey.isPressed)
         {
-            transform.Translate(-0.1f, 0, 0);
+            transform.Translate(-0.1f * speedy, 0, 0);
         }
+
 
         // Dキーで右に移動
         if (Keyboard.current.dKey.isPressed)
         {
-            transform.Translate(0.1f, 0, 0);
+            transform.Translate(0.1f * speedy, 0, 0);
         }
 
         // Wキーで上に移動
         if (Keyboard.current.wKey.isPressed)
         {
-            transform.Translate(0, 0.1f, 0);
+            transform.Translate(0, 0.1f * speedy, 0);
         }
 
         // Sキーで下に移動
         if (Keyboard.current.sKey.isPressed)
         {
-            transform.Translate(0, -0.1f, 0);
+            transform.Translate(0, -0.1f * speedy, 0);
         }
     }
 }
