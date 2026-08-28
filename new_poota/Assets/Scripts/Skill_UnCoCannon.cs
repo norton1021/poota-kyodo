c32d388ef5cd35b72a24a285cdbd77b7992ff795
Keiryu14 <ichitariyk1499@gmail.com>
2026-08-28 14:47:27 +0900

    uncoキャノン変更

--- new_poota/Assets/Scripts/Skill_UnCoCannon.cs
+++ new_poota/Assets/Scripts/Skill_UnCoCannon.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 public class Skill_UnCoCannon : MonoBehaviour
 {
-    Vector3 rotation = new Vector3(0, 0, 90);
-    int count = 0;
+    Vector3 rotation = new Vector3(0, 0, 45);
     GameObject player;
 
     void Start()
     {
-       count++;
+        rotation *= Time.deltaTime;
+        transform.Rotate(rotation);
     }
 
     void Update()
     {
         this.player = GameObject.FindGameObjectWithTag("Player");
-        transform.Rotate(rotation * Time.deltaTime);
+        transform.position += rotation;
     }
 }
