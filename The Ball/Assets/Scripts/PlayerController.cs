c6c60d4228fe447b123ffd65117be2ffec8f846a
Keiryu14 <ichitariyk1499@gmail.com>
2026-09-14 14:49:31 +0900

    プレイヤーが移動するためのスクリプトを作成

---
+++ The Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 600.0f;
+    float walkForce = 30;
+    float maxWalkSpeed = 2.0f;
+    
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        // ジャンプ
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        // 左に移動
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            if (this.rigid2D.linearVelocityX < this.maxWalkSpeed)
+            {
+                this.rigid2D.AddForce(-transform.right * this.walkForce);
+            }
+        }
+
+        // 右に移動
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            if (this.rigid2D.linearVelocityX < this.maxWalkSpeed)
+            {
+                this.rigid2D.AddForce(transform.right * this.walkForce);
+            }
+        }
+    }
+}
