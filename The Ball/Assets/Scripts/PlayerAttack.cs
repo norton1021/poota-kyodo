5413f563d246d023679682f972c9d27427aebc08
norton1021 <renhiso1209@icloud.com>
2026-09-16 16:41:41 +0900

    stage3完成

--- The Ball/Assets/Scripts/PlayerAttack.cs
+++ The Ball/Assets/Scripts/PlayerAttack.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
+    public float stompBounce = 8f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
             Destroy(collision.gameObject);
+            Rigidbody2D playerRb = GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, stompBounce);
+            }
             Debug.Log("ìGÇì|ÇµÇΩÅI");
-
         }
     }
 }
